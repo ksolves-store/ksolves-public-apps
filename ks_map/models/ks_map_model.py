c57725e10cd51c2fc283ac91d446cb96c8ddb9da
ajay-ksolves <ajay.kumar@ksolves.com>
2021-09-30 17:50:16 +0530

    Set default zoom level

--- ks_map/models/ks_map_model.py
+++ ks_map/models/ks_map_model.py
@@ -6,5 +6,5 @@ class KsResCompany(models.Model):
 
     ks_latitude = fields.Char('Latitude', required=True)
     ks_longitude = fields.Char('Longitude', required=True)
-    ks_zoom = fields.Integer('Zoom Level', required=True, help="Zoom level is integer value between 10-18")
+    ks_zoom = fields.Integer('Zoom Level', required=True, help="Zoom level is integer value between 10-18",default=15)
 

debfeb9869463761b4926ed7a67296585e72de02
ajay-ksolves <ajay.kumar@ksolves.com>
2021-09-30 17:47:28 +0530

    Checking object length

--- ks_map/static/src/js/ks_map.js
+++ ks_map/static/src/js/ks_map.js
@@ -5,7 +5,7 @@ odoo.define('Ks_Map.map', function (require) {
     var ajax = require('web.ajax');
     ajax.jsonRpc("/ks_get_map_cod", 'call', {}, {
     }).then(function (cod){
-        if(cod.length) {
+        if(Object.keys(cod).length) {
             var ks_point = new L.LatLng(parseFloat(cod.lan),parseFloat(cod.lon));
             var ks_mymap = L.map('mapid').setView(ks_point, parseInt(cod.zoom));
             L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGV2dnZ2dnZ2diIsImEiOiJja3Nvb2Z1cHkwNnhxMnNzNmNsNTAxZ2d2In0.2LMgt_oPT8XSFQ6IlEjLkw',

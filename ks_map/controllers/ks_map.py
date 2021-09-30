ffbe5257ee206816bc737bf1cced4755182c4d82
ajay-ksolves <ajay.kumar@ksolves.com>
2021-09-30 17:24:24 +0530

    Added no co-ordinate case

--- ks_map/controllers/ks_map.py
+++ ks_map/controllers/ks_map.py
@@ -1,13 +1,15 @@
 from odoo import http
 from odoo.http import request
 
+
 class KsMapCod(http.Controller):
 
-   @http.route('/ks_get_map_cod', type='json', auth='public', website=True)
-   def ks_map_details(self, **kwargs):
-       ks_co_ordinate = request.env.company
-       if ks_co_ordinate.ks_longitude and ks_co_ordinate.ks_latitude:
-           cod = {'lon': ks_co_ordinate.ks_longitude,
-                  'lan': ks_co_ordinate.ks_latitude,
-                  'zoom': ks_co_ordinate.ks_zoom}
-           return cod
+    @http.route('/ks_get_map_cod', type='json', auth='public', website=True)
+    def ks_map_details(self, **kwargs):
+        ks_co_ordinate = request.env.company
+        cod = dict()
+        if ks_co_ordinate and ks_co_ordinate.ks_longitude and ks_co_ordinate.ks_latitude:
+            cod = {'lon': ks_co_ordinate.ks_longitude,
+                   'lan': ks_co_ordinate.ks_latitude,
+                   'zoom': ks_co_ordinate.ks_zoom}
+        return cod

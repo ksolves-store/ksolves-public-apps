521a875ff3fb24fbd875cae5ff7069a0d57aeebe
Gauravks321 <gaurav.khare@ksolves.com>
2022-02-17 10:00:01 +0530

    Stock Access Rule updated code 1. Added admin bydefault in many2many fields 2. Added Super User 3. Remove commented code

--- ks_stock_location_access_restriction/models/__init__.py
+++ ks_stock_location_access_restriction/models/__init__.py
@@ -2,4 +2,3 @@
 
 from . import ks_stock_location_extension
 from . import ks_res_users_extension
-# from . import ks_stock_picking_type_extension
--- ks_stock_location_access_restriction/models/ks_res_users_extension.py
+++ ks_stock_location_access_restriction/models/ks_res_users_extension.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
+from odoo import api, Command, fields, models, _
 
 
 class KsResUsersExtension(models.Model):
@@ -10,11 +10,10 @@ class KsResUsersExtension(models.Model):
 
     def write(self, vals):
         ks_before_write_vals = self.ks_default_location_ids
-        print(ks_before_write_vals)
         res = super(KsResUsersExtension, self).write(vals)
         ks_after_write_vals = self.ks_default_location_ids
         for loc_id in ks_before_write_vals:
             if not loc_id in ks_after_write_vals:
-                ks_get_loc_id = self.env['stock.location'].search([('id','=',loc_id.id)],limit=1)
-                ks_get_loc_id.ks_own_user_ids = [(3,self.env.user.id)]
+                ks_get_loc_id = self.env['stock.location'].search([('id', '=', loc_id.id)], limit=1)
+                ks_get_loc_id.ks_own_user_ids = [Command.unlink(self.env.user.id)]
         return res
